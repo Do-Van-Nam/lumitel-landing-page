70417e83b19d0ff5565d695920e61861c5600432
Do-Van-Nam <nam15sep2003@gmail.com>
2025-05-28 16:19:12 +0700

    1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2154317415"
+    serviceWorkerVersion: "3246628266"
   }
 });
